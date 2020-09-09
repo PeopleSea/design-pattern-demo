13ccafe0512c5828fee98ae3b637e2da1708c061
PeopleSea <season7300@gmail.com>
2020-09-09 15:20:16 +0800

    no message

---
+++ src/main/java/abs/factory/AbstractFactory.java
@@ -0,0 +1,8 @@
+package abs.factory;
+
+/**
+ * @author by Eric
+ * @since 2020/9/9 下午 02:43
+ */
+public class AbstractFactory {
+}
---
+++ src/main/java/abs/factory/FactoryProducer.java
@@ -0,0 +1,8 @@
+package abs.factory;
+
+/**
+ * @author by Eric
+ * @since 2020/9/9 下午 02:55
+ */
+public class FactoryProducer {
+}
